6b544b716592da3cc2505c65cd13c340d5259946
Anton Kulikov <kulikusya@gmail.com>
2022-10-26 14:33:08 +0300

    solution for sortStrings

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,12 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
+    const directions = new Map([['asc', 1], ['desc', -1]]);
+    if (!directions.has(param)) {
+        throw new Error(`Unknown parameter 'param': '${param}' !`);
+    }
 
+    return [...arr].sort((s1, s2) => {
+        return directions.get(param) * s1.localeCompare(s2, ['ru', 'en'], {caseFirst: 'upper'})
+    });
 }
