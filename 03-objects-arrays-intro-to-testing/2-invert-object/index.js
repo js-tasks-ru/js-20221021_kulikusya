7cb9332e04d2a675061ea494e69ebca2b510e339
Anton Kulikov <kulikusya@gmail.com>
2022-10-31 20:56:31 +0300

    solution for 'invertObj' added additional tests

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -5,4 +5,10 @@
  */
 export function invertObj(obj) {
 
+    if (obj === null || typeof(obj) !== 'object') {
+        return;
+    }
+
+    return Object.fromEntries(Object.entries(obj).map(([key, value]) => [value, key]));
+
 }
