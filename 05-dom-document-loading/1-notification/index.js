4dd27e97a4819c071f00ffd2a9ca6e7cb20c863c
Anton Kulikov <kulikusya@gmail.com>
2022-11-08 21:12:11 +0300

    NotificationMessage

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,72 @@
 export default class NotificationMessage {
 
+    static currentNotification;
+    static destroyCurrentNotification() {
+        if(NotificationMessage.currentNotification) {
+            NotificationMessage.currentNotification.destroy();
+        }
+    }
+    
+
+    constructor(message = '', {duration = 0, type = ''} = {}) {
+        this._message = message;
+        this._duration = duration;
+        this._type = type;
+        
+        this.render();
+    }
+
+    get duration() {
+        return this._duration;
+    }
+
+    getType() {
+        return this._type;
+    }
+    getMessage() {
+        return this._message;
+    }
+
+    show(parentElement = document.body) {
+        NotificationMessage.destroyCurrentNotification();
+
+        parentElement.append(this.element);
+        NotificationMessage.currentNotification = this;
+        setTimeout(() => {
+            this.destroy();
+        }, this.duration);
+    }
+
+    render() {
+        const elementWrapper = document.createElement('div');
+        elementWrapper.innerHTML = this.getTemplate();
+        
+        this.element = elementWrapper.firstElementChild;
+    }
+
+    getTemplate() {
+        return `
+  <div class="notification ${this.getType()}" style="--value:${this.duration/1000}s">
+    <div class="timer"></div>
+    <div class="inner-wrapper">
+      <div class="notification-header">${this.getType()}</div>
+      <div class="notification-body">
+      ${this.getMessage()}
+      </div>
+    </div>
+  </div>
+        `;
+    }
+
+    remove() {
+        if (this.element) {
+            this.element.remove();
+        }
+    }
+
+    destroy() {
+        this.remove();
+        this.element = null;
+    }
+
 }
